f9e32d568e9c3a88643e117d64c284ad38798152
Toru Honda <dboy.hazer231@gmail.com>
2021-07-28 13:07:12 +0900

    Translate Japanese in the java files under /src to English.

--- src/main/java/blanco/veevalidate/task/BlancoVeeValidateProcessImpl.java
+++ src/main/java/blanco/veevalidate/task/BlancoVeeValidateProcessImpl.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class BlancoVeeValidateProcessImpl implements BlancoVeeValidateProcess {
 
     /**
-     * メッセージ。
+     * A message.
      */
     private final BlancoVeeValidateMessage fMsg = new BlancoVeeValidateMessage();
 
@@ -43,7 +43,7 @@ public class BlancoVeeValidateProcessImpl implements BlancoVeeValidateProcess {
             }
 
             /*
-             * 改行コードを決定します。
+             * Determines the newline code.
              */
             String LF = "\n";
             String CR = "\r";
@@ -75,20 +75,20 @@ public class BlancoVeeValidateProcessImpl implements BlancoVeeValidateProcess {
             }
 
             /*
-             * targetdir, targetStyleの処理。
-             * 生成されたコードの保管場所を設定する。
+             * Processes targetdir and targetStyle.
+             * Sets the storage location for the generated code.
              * targetstyle = blanco:
-             *  targetdirの下に main ディレクトリを作成
+             *  Creates a main directory under targetdir.
              * targetstyle = maven:
-             *  targetdirの下に main/java ディレクトリを作成
+             *  Creates a main/java directory under targetdir.
              * targetstyle = free:
-             *  targetdirをそのまま使用してディレクトリを作成。
-             *  ただしtargetdirがからの場合はデフォルト文字列(blanco)使用する。
+             *  Creates a directory using targetdir as is.
+             *  However, the default string (blanco) is used if targetdir is empty.
              * by tueda, 2019/08/30
              */
             String strTarget = input.getTargetdir();
             String style = input.getTargetStyle();
-            // ここを通ったら常にtrue
+            // Always true when passing through here.
             boolean isTargetStyleAdvanced = true;
             if (style != null && BlancoVeeValidateConstants.TARGET_STYLE_MAVEN.equals(style)) {
                 strTarget = strTarget + "/" + BlancoVeeValidateConstants.TARGET_DIR_SUFFIX_MAVEN;
@@ -96,12 +96,12 @@ public class BlancoVeeValidateProcessImpl implements BlancoVeeValidateProcess {
                     !BlancoVeeValidateConstants.TARGET_STYLE_FREE.equals(style)) {
                 strTarget = strTarget + "/" + BlancoVeeValidateConstants.TARGET_DIR_SUFFIX_BLANCO;
             }
-            /* style が free だったらtargetdirをそのまま使う */
+            /* Uses targetdir as is if style is free. */
             if (input.getVerbose()) {
                 System.out.println("/* tueda */ TARGETDIR = " + strTarget);
             }
 
-            // テンポラリディレクトリを作成。
+            // Creates a temporary directory.
             new File(input.getTmpdir()
                     + BlancoVeeValidateConstants.TARGET_SUBDIRECTORY).mkdirs();
 
@@ -109,22 +109,22 @@ public class BlancoVeeValidateProcessImpl implements BlancoVeeValidateProcess {
                     .getTmpdir()
                     + BlancoVeeValidateConstants.TARGET_SUBDIRECTORY);
 
-            // XML化されたメタファイルからValueObjectを生成
-            // 最初にテンポラリフォルダを走査
+            // Generates ValueObject from XML-ized metafiles.
+            // Scans the temporary folder first.
             final File[] fileMeta2 = new File(input.getTmpdir()
                     + BlancoVeeValidateConstants.TARGET_SUBDIRECTORY)
                     .listFiles();
 
         /*
-         * まず始めにすべてのシートを検索して，クラス名とpackage名のリストを作ります．
-         * php形式の定義書では，クラスを指定する際にpackage名が指定されていないからです．
+         * First, searches all the sheets and makes a list of class and package names.
+         * This is because the package name is not specified when specifying a class in the PHP format definition.
          */
             BlancoVeeValidateUtil.isVerbose = input.getVerbose();
             BlancoVeeValidateUtil.processValueObjects(input);
 
             List<BlancoVeeValidateClassStructure> validatorStructures = new ArrayList<>();
 
-            // 次にメタディレクトリとして指定されているディレクトリを走査
+            // Next, scans the directory specified as the meta directory.
             for (int index = 0; index < fileMeta2.length; index++) {
                 if (fileMeta2[index].getName().endsWith(".xml") == false) {
                     continue;
@@ -135,7 +135,7 @@ public class BlancoVeeValidateProcessImpl implements BlancoVeeValidateProcess {
                 final BlancoVeeValidateClassStructure[] structures = parser.parse(fileMeta2[index]);
 
                 /*
-                 * いったん、全部集めます
+                 * Collects them all.
                  */
                 for (int index2 = 0; index2 < structures.length; index2++) {
                     BlancoVeeValidateClassStructure classStructure = structures[index2];
@@ -144,7 +144,7 @@ public class BlancoVeeValidateProcessImpl implements BlancoVeeValidateProcess {
             }
 
             /*
-             * そして、ValidateInitializer クラスを生成します。
+             * Then, generates the ValidateInitializer class.
              */
             final BlancoVeeValidateXml2TypeScriptClass xml2Class = new BlancoVeeValidateXml2TypeScriptClass();
             xml2Class.setEncoding(input.getEncoding());
@@ -157,7 +157,7 @@ public class BlancoVeeValidateProcessImpl implements BlancoVeeValidateProcess {
 
             return BlancoVeeValidateBatchProcess.END_SUCCESS;
         } catch (TransformerException e) {
-            throw new IOException("XML変換の過程で例外が発生しました: " + e.toString());
+            throw new IOException("An exception occurred during the XML conversion process: " + e.toString());
         }
     }
 
