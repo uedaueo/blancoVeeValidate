7417f9e1b8b6b408624764303e2dd4d2c542f38e
tueda <ueo@ueo.co.jp>
2022-01-18 18:25:03 +0900

    0.1.3: JUnit is updated to 4.13.2 because of security issue.

--- blanco/main/blanco/veevalidate/task/BlancoVeeValidateTask.java
+++ blanco/main/blanco/veevalidate/task/BlancoVeeValidateTask.java
@@ -367,7 +367,7 @@ public class BlancoVeeValidateTask extends Task {
     public final void execute() throws BuildException {
         System.out.println("BlancoVeeValidateTask begin.");
 
-        // 項目番号[1]、アトリビュート[metadir]は必須入力です。入力チェックを行います。
+        // 項目番号[1], アトリビュート[metadir]は必須入力です。入力チェックを行います。
         if (fIsFieldMetadirProcessed == false) {
             throw new BuildException("必須アトリビュート[metadir]が設定されていません。処理を中断します。");
         }
@@ -388,10 +388,10 @@ public class BlancoVeeValidateTask extends Task {
 
         try {
             // 実際のAntタスクの主処理を実行します。
-            // この箇所でコンパイルエラーが発生する場合、BlancoVeeValidateProcessインタフェースを実装して blanco.veevalidate.taskパッケージに BlancoVeeValidateProcessImplクラスを作成することにより解決できる場合があります。
+            // If you get a compile error at this point, You may be able to solve it by implementing a BlancoVeeValidateProcess interface and creating an BlancoVeeValidateProcessImpl class in package blanco.veevalidate.task.
             final BlancoVeeValidateProcess proc = new BlancoVeeValidateProcessImpl();
             if (proc.execute(fInput) != BlancoVeeValidateBatchProcess.END_SUCCESS) {
-                throw new BuildException("タスクは異常終了しました。");
+                throw new BuildException("The task has terminated abnormally.");
             }
         } catch (IllegalArgumentException e) {
             if (getVerbose()) {
